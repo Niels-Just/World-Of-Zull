bd357700bbec0635e04eb23f419742e07341b859
niels-just <nison24@student.sdu.dk>
2024-11-05 09:28:50 +0100

    new and updated

--- World Of Zuul 2.1/obj/Debug/net8.0/World Of Zuul 2.1.AssemblyInfo.cs
+++ World Of Zuul 2.1/obj/Debug/net8.0/World Of Zuul 2.1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("World Of Zuul 2.1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e88ec73638295dcc517bed82598e99395bcff98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5a8bce23d4926ecabc1de9ec0534a1382580526")]
 [assembly: System.Reflection.AssemblyProductAttribute("World Of Zuul 2.1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("World Of Zuul 2.1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
