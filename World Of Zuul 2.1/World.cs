8411a017175db763b547d709f0e06c3d70af9406
kaspar1910 <kaspar1910@gmail.com>
2024-11-05 12:30:30 +0100

    hej

--- World Of Zuul 2.1/World.cs
+++ World Of Zuul 2.1/World.cs
@@ -3,52 +3,12 @@ namespace World_Of_Zuul;
  */
 
 class World {
-    //starting room
-    Space Baghave;
+    
   
     //creating serval 'Space' objects and defining the connections between them.
-    public World () {
-        Space Baghave    = new Space("Baghave");
-<<<<<<< HEAD
-        Space Glad_Nabo = new Space("Glad_Nabo");
-        Space Sur_Nabo    = new Space("Sur_Nabo");
-        Space Vej_Vest     = new Space("Vej_Vest");
-        Space Vej_Øst      = new Space("Vej_øst");
-        Space Vej_Midte  = new Space("Vej_Midte");
-        Space Glas_Mager_Glenn       = new Space("Glas_Mager_Glenn");
-        Space Elektriker_Erik     = new Space("Elektriker_Erik");
-        Space Kunst_Haven      = new Space("Kunst_Haven");
-    
-        //defines how these spaces are conected
-        Baghave.AddEdge("Nord" , Vej_Midte);
-        Baghave.AddEdge("Øst", Sur_Nabo);
-        Baghave.AddEdge("Vest", Glad_Nabo);
-        Glad_Nabo.AddEdge("Øst", Baghave);
-        Glad_Nabo.AddEdge("Nord", Vej_Vest);
-        Sur_Nabo.AddEdge("Nord", Vej_Øst);
-        Sur_Nabo.AddEdge("Vest", Baghave);
-        Vej_Vest.AddEdge("Syd", Glad_Nabo);
-        Vej_Vest.AddEdge("Nord", Glas_Mager_Glenn);
-        Vej_Vest.AddEdge("Øst", Vej_Midte);
-        Vej_Midte.AddEdge("Syd", Baghave);
-        Vej_Midte.AddEdge("Øst", Vej_Øst);
-        Vej_Midte.AddEdge("Vest", Vej_Vest);
-        Vej_Midte.AddEdge("Nord", Elektriker_Erik);
-        Vej_Øst.AddEdge("Nord", Kunst_Haven);
-        Vej_Øst.AddEdge("Syd", Sur_Nabo);
-        Vej_Øst.AddEdge("Vest", Vej_Midte);
-        Glas_Mager_Glenn.AddEdge("Syd", Vej_Vest);
-        Glas_Mager_Glenn.AddEdge("Øst", Elektriker_Erik);
-        Elektriker_Erik.AddEdge("Vest", Glas_Mager_Glenn);
-        Elektriker_Erik.AddEdge("Syd", Vej_Midte);
-        Elektriker_Erik.AddEdge("Øst", Kunst_Haven);
-        Kunst_Haven.AddEdge("Syd", Vej_Øst);
-        Kunst_Haven.AddEdge("Vest", Elektriker_Erik);        
-        
-        
-        //starts at this location
-        this.entry = Baghave;
-=======
+    public World ()
+    {
+        Space Baghave = new Space("Baghave");
         Space Solvej32 = new Space("Solvej32");
         Space Solvej28   = new Space("Solvej28");
         Space VejMidt     = new Space("Vej midt");
@@ -93,8 +53,7 @@ class World {
         Solvej33.AddEdge("Syd",VejØst);
     
         //starts at this location
-        this.Baghave = Baghave;
->>>>>>> refs/remotes/origin/main
+        this.entry = Baghave;
         
         // Create and add items to room
         /* Item solar_cells = new Item("SolarCells", "Converts energy from light directly into electricity");
@@ -113,7 +72,7 @@ class World {
         cave.AddRoomItem(glass_plane);
         home.AddRoomItem(battery);
         */
->>>>>>> refs/remotes/origin/main
+
     }
   
     //allows access for the rest of the program
