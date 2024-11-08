35f267ff39784afb6231ff1f739bb06f2a12a5a4
niels-just <nison24@student.sdu.dk>
2024-11-08 12:08:18 +0100

    NPCs ?

--- World Of Zuul 2.1/obj/Debug/net8.0/World Of Zuul 2.1.AssemblyInfo.cs
+++ World Of Zuul 2.1/obj/Debug/net8.0/World Of Zuul 2.1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("World Of Zuul 2.1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94d8427eeb13a0f568676ffbb3382fb0c5e03893")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+455941e45887ab7c3cbac7e5a39cd0550410beff")]
 [assembly: System.Reflection.AssemblyProductAttribute("World Of Zuul 2.1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("World Of Zuul 2.1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
