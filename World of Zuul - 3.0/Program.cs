e051d5674e59ecf487b3caff33a0516f547760b1
niels-just <nison24@student.sdu.dk>
2024-11-12 11:28:01 +0100

    New World of Zuul Implementation

--- World of Zuul - 3.0/Program.cs
+++ World of Zuul - 3.0/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Room room1 = new Room("a dark cave");
+        Room room2 = new Room("a house");
+        Room room3 = new Room("a forrest");
+        Room room4 = new Room("a graveyard");
+        
+        room1.SetExit("north",room2);
+        room2.SetExit("south",room1);
+        room2.SetExit("east",room3);
+        room3.SetExit("north",room2);
+        
+        //Starter spillet
+        Game game = new Game(room1);
+        game.ShowCurrentRoom();
+
+        while (true)
+        {
+            Console.WriteLine("Enter a direction (north,south,east,west) or 'exit' to quit:");
+            string input = Console.ReadLine();
+            if (input.ToLower() == "exit")
+            {
+                break;
+            }
+            game.Move(input.ToLower());
+        }
     }
 }
