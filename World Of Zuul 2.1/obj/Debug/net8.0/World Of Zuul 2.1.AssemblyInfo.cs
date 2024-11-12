cbe2b11b99058edd48795b8d41cf4efe41b3c82e
kaspar1910 <kaspar1910@gmail.com>
2024-11-12 09:34:12 +0100

    hej

--- World Of Zuul 2.1/obj/Debug/net8.0/World Of Zuul 2.1.AssemblyInfo.cs
+++ World Of Zuul 2.1/obj/Debug/net8.0/World Of Zuul 2.1.AssemblyInfo.cs
@@ -13,11 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("World Of Zuul 2.1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+195d74da2235b8b35a5c24c85fea9e9f05cf588c")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97e94616084741e17ad83c4425f80b1777db0c0f")]
->>>>>>> refs/remotes/origin/main
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9388351b663e2a3137336b2dd4759deaf07980a")]
 [assembly: System.Reflection.AssemblyProductAttribute("World Of Zuul 2.1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("World Of Zuul 2.1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
