14d36f404207cd3390ee332f52741f9688c3fd0d
niels-just <nison24@student.sdu.dk>
2024-11-22 10:38:45 +0100

    prøvede at tilføje så man kan bygge

--- World of Zuul - 3.0/Commands.cs
+++ World of Zuul - 3.0/Commands.cs
@@ -92,7 +92,7 @@ namespace World_of_Zuul___3._0
         
         public void Assemble(Player player)
         {
-            if (player.HasItem("Frame, Glass")) // Indsæt nødvendige items her
+            if (player.HasItem("part1,part2,part3,part4,part5,part6,part,7,par8")) // Indsæt nødvendige items her
             {
                 //Console.WriteLine("You've sucessfully assembled a solar panel!");
                 Item solar_panel = new Item("Sopanel", "Producere vedvarende energi!");
--- World of Zuul - 3.0/obj/Debug/net8.0/World of Zuul - 3.0.AssemblyInfo.cs
+++ World of Zuul - 3.0/obj/Debug/net8.0/World of Zuul - 3.0.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("World of Zuul - 3.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+543d5f5c2e274724b97d7c238ecdd89a450e5e71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd9ecf3b1dc3f8f2ab38279fb595aa77580aea99")]
 [assembly: System.Reflection.AssemblyProductAttribute("World of Zuul - 3.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("World of Zuul - 3.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
