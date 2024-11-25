cb34453b988283b001b6a7a00deeae5bf01de4e7
niels-just <nison24@student.sdu.dk>
2024-11-25 22:27:45 +0100

    updated main metode, delt op i data, domain og presentation, 3 lags arkitektur

--- World of Zuul - 3.0/obj/Debug/net8.0/World of Zuul - 3.0.AssemblyInfo.cs
+++ World of Zuul - 3.0/obj/Debug/net8.0/World of Zuul - 3.0.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("World of Zuul - 3.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13fbfe63f24cafb0d2a3c3f0ffcea116b0f48e3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1daeab07ced388cb68e9084c7086254ace010e9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("World of Zuul - 3.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("World of Zuul - 3.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
