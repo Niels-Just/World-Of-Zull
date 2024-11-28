bcfd8c7041ef164a9024705976fb56d0816662f6
Mads Fossum <madsfossum@outlook.dk>
2024-11-28 14:50:02 +0100

    CurrentDirectory fix

--- World Of Zull 4.0/World-Of-Zull-4.0/obj/Debug/net8.0/World-Of-Zull-4.0.AssemblyInfo.cs
+++ World Of Zull 4.0/World-Of-Zull-4.0/obj/Debug/net8.0/World-Of-Zull-4.0.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("World-Of-Zull-4.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b1492f21734fe73ae710615159195d090b5b8a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("World-Of-Zull-4.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("World-Of-Zull-4.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
